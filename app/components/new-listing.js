f572a6438db08a45cad23f71ab088cfa15c77ea2
maldon meehan and sky rousse <maldon@>
2016-08-24 13:42:28 -0700

    add component for display cart and a service for constructing shopping cart

---
+++ app/components/new-listing.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    saveListing() {
+      var params = {
+        name: this.get('name') ? this.get('name'): "",
+        description: this.get('description')  ? this.get('description'): "",
+        location: this.get('location')  ? this.get('location'): "",
+        pricePerDay: this.get('pricePerDay')  ? this.get('pricePerDay'): "",
+        image: this.get('image') ? this.get('image'):"https://pixabay.com/static/uploads/photo/2012/04/10/22/34/flat-26677_960_720.png",
+        rating: 0,
+        category: this.get('category')
+      };
+      this.sendAction('saveListing', params);
+    }
+  }
+});
--- app/routes/category.js
+++ app/routes/category.js
@@ -4,4 +4,15 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('category', params.category_id);
   },
+  actions: {
+    saveListing(params) {
+      debugger;
+      var newListing = this.store.createRecord('listing', params);
+      var category = params.category;
+      category.get('listings').addObject(newListing);
+      newListing.save().then(function() {
+        return category.save();
+      });
+    }
+  }
 });
